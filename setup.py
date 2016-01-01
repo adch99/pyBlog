028f718c539790fd076e8a96069f958cd97852ea
aditya chincholi <aditya@Latitude-D620.org>
2016-01-01 18:33:05 +0530

    setup and html

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ config = {
 	'download_url': 'Where to download it',
 	'author_email': 'adityachincholi@gmail.com',
 	'version': '0.1',
-	'install_requires': ['nose', 'web.py'],
+	'install_requires': ['nose', 'web.py', 'markdown'],
 	'packages': ['blog'],
 	'scripts': []
 	'name': 'Blog'
